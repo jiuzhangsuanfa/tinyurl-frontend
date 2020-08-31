3c5b645813bb8e26498d7f570d479f62792a2917
a-4 <a-4@don.red>
2020-09-01 00:11:13 +0800

    fix(component/module): fix export

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -1,11 +1,8 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   declarations: [LoadingComponent],
-  imports: [
-    CommonModule
-  ]
+  exports: [LoadingComponent]
 })
 export class ComponentModule { }
