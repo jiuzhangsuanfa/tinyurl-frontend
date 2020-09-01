35c56d5d791a0616b6e922a78e3438120d08ded9
a-4 <a-4@don.red>
2020-09-02 00:47:44 +0800

    chore(angular): add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentModule } from './component/component.module';
@@ -13,7 +14,8 @@ import { PageModule } from './page/page.module';
     BrowserModule,
     AppRoutingModule,
     ComponentModule,
-    PageModule
+    PageModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
