ba70620fe21617bb2572b1d68ce5ff14fc729d98
a-4 <a-4@don.red>
2020-09-03 01:46:34 +0800

    feat(page/module): import module

--- src/app/page/page.module.ts
+++ src/app/page/page.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ComponentModule } from '../component/component.module';
 import { MaterialModule } from '../module/material.module';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [MaterialModule],
+  imports: [
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ComponentModule
+  ],
   exports: [HomeComponent]
 })
 export class PageModule { }
