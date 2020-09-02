e6777958c68e039ceb55d9c80cf706941ee8aef6
a-4 <a-4@don.red>
2020-09-03 01:46:58 +0800

    feat(home): update style & input component

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+const URL_REG = /^((https|http|ftp|rtsp|mms):\/\/)(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?(([0-9]{1,3}.){3}[0-9]{1,3}|([0-9a-z_!~*'()-]+.)*([0-9a-z][0-9a-z-]{0,61})?[0-9a-z].[a-z]{2,6})(:[0-9]{1,4})?((\/?)|(\/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+\/?)$/;
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  icon: 'sync_alt' | 'arrow_back' | 'arrow_forward' = 'sync_alt';
+  form: FormGroup;
 
-  ngOnInit(): void {
+  constructor() {
+    this.form = new FormGroup({
+      url: new FormControl(
+        undefined,
+        [
+          Validators.required,
+          Validators.minLength(0),
+          Validators.maxLength(4096),
+          // url pattern
+          Validators.pattern(URL_REG)
+        ]
+      )
+    });
+  }
+
+  ngOnInit() { }
+
+  onInput() {
+    const match = this.form.get('url').value.match(URL_REG);
+    if (!match) {
+      this.icon = 'sync_alt';
+    } else if (!match[5] || match[5] !== 's.don.red') {
+      this.icon = 'arrow_forward';
+    } else {
+      this.icon = 'arrow_back';
+    }
   }
 
 }
