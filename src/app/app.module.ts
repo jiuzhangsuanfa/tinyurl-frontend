f5dd1e94f532f37d282d584e10cbf116e26d02d5
a-4 <a-4@don.red>
2020-09-03 02:04:42 +0800

    feat(app): update header logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,6 +16,7 @@ import { PageModule } from './page/page.module';
     AppRoutingModule,
     ComponentModule,
     PageModule,
+    MatTooltipModule,
     BrowserAnimationsModule
   ],
   providers: [],
