f92c5a7d0bb1d9e7bc04b11c072c5899ce403221
a-4 <a-4@don.red>
2020-09-02 17:57:24 +0800

    feat(module): add material module

--- src/app/page/page.module.ts
+++ src/app/page/page.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '../module/material.module';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [HomeComponent],
+  imports: [MaterialModule],
   exports: [HomeComponent]
 })
 export class PageModule { }
