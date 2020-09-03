19ade0c6afc48d200eb7538a0bc65a814e479e03
a-4 <a-4@don.red>
2020-09-03 08:50:45 +0800

    perf(home): optimize home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home.component';
+import { PolicyComponent } from './page/policy/policy.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'policy', component: PolicyComponent }
 ];
 
 @NgModule({
