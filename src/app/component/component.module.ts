2df22a7fe637b77ca84481a3832ad634d7920f8a
a-4 <a-4@don.red>
2020-09-05 18:10:23 +0800

    feat(component/loading): add loading color

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   declarations: [LoadingComponent],
+  imports: [CommonModule],
   exports: [LoadingComponent]
 })
 export class ComponentModule { }
